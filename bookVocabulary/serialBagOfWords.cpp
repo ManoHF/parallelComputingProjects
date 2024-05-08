5981b1e62a22912d4305bb542bbac47ebbb8d42a
ManoHF <manuel22hf@gmail.com>
2024-05-07 23:44:58 -0600

    serial bag of words

---
+++ bookVocabulary/serialBagOfWords.cpp
@@ -0,0 +1,68 @@
+#include <string>
+#include <iostream>
+#include <map>
+#include <set>
+#include <fstream>
+#include <vector>
+
+void read_csv(const std::string &filename, std::map<std::string, int> &count, std::set<std::string> &vocabulary);
+void write_bag_of_words(const std::string& filename, std::vector<std::map<std::string, int>> &counts, std::set<std::string> vocabulary);
+
+int main(int argc, char* argv[]) {
+
+    if (argc == 1) {
+        std::cout << "Please include the names of the files to read";
+        return -1;
+    }
+
+    int nBooks = argc - 1;
+    std::vector<std::map<std::string, int>> counts(nBooks);
+    std::set<std::string> vocabulary;
+    std::string path;
+
+    for (int i = 1; i <= nBooks; i++) {
+        std::string name = argv[i];
+        path = "./books/" + name + ".txt";
+
+        read_csv(path, counts[i - 1], vocabulary);
+    }
+
+    std::string out_file = "bag_of_words.txt";
+    write_bag_of_words(out_file, counts, vocabulary);
+    return 0;
+}
+
+void read_csv(const std::string &filename, std::map<std::string, int> &count, std::set<std::string> &vocabulary) {
+    std::ifstream file(filename);
+    std::string word;
+
+    while (std::getline(file, word, ',')) {
+        if (count.find(word) == count.end()) {
+            count[word] = 0;
+        }
+
+        count[word]++;
+        vocabulary.insert(word);
+    }
+}
+
+void write_bag_of_words(const std::string& filename, std::vector<std::map<std::string, int>> &counts, std::set<std::string> vocabulary) {
+    std::ofstream file(filename);
+
+    for (std::string elem : vocabulary) {
+        file << elem << ",";
+    }
+    file << "\n";
+
+    for (const auto &count_map : counts) {
+        for (const std::string &elem : vocabulary) {
+            if (count_map.find(elem) != count_map.end()) {
+                file << count_map.at(elem) << ",";
+            }
+            else {
+                file << "0" << ",";
+            }
+        }
+        file << "\n";
+    }
+}
